2c3eb429830d186a5687e9be399ee2503f274f37
Muradovvv <muradovv.murad2706@gmail.com>
2024-11-27 01:56:39 +0400

    file send

--- js-axios/assets/js/main.js
+++ js-axios/assets/js/main.js
@@ -5,32 +5,30 @@ const productCard = document.querySelector(".row");
 
 let products = null;
 
-async function GetData(){
-  
-    const res = await axios.get(`${BASE_URL}/products`)
-    products = res.data;
-    console.log(res.data);
-    drawCard(res.data)
+async function GetData() {
+  const res = await axios.get(`${BASE_URL}/products`);
+  products = res.data;
+  console.log(res.data);
+  drawCard(res.data);
 }
-GetData()
+GetData();
 
-function drawCard(products){
-    productCard.innerHTML="";
-    products.forEach(product => {
-        productCard.innerHTML+=`
+function drawCard(products) {
+  productCard.innerHTML = "";
+  products.forEach((product) => {
+    productCard.innerHTML += `
         <div class="col-12 col-md-6 col-lg-4 allproducts">
         <div class="card" style="width: 18rem;">
         <img src="${product.image}"
             <div class="card-body">
               <h5 class="card-name">${product.name}</h5>
               <p class="card-desc">${product.description}</p>
-              <p> $${product.price}</p>
+              <p class="price"> $${product.price}</p>
             </div>
           </div>
     </div>
         `;
-        
-    });
+  });
 }
 
 const searchInput = document.querySelector(".search");
