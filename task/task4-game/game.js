f02830120a91bb063e9d72d4eae9a7b7bef7dc49
Muradovvv <muradovv.murad2706@gmail.com>
2024-12-17 15:46:18 +0400

    file add

--- task/task4-game/game.js
+++ task/task4-game/game.js
@@ -1,5 +1,6 @@
 let count = 0;
-let inc = 1
+let inc = 1;
+
 const counter = document.getElementById('counter');
 const img = document.querySelector('.img');
 const lv1 = document.querySelector('.lv1');
@@ -7,42 +8,37 @@ const lv2 = document.querySelector('.lv2');
 const lv3 = document.querySelector('.lv3');
 
 img.addEventListener('click', function () {
-    count= count +inc;
+    count = count + inc;
     counter.textContent = count;
+    img.classList.add('animate');
+    setTimeout(() => {
+        img.classList.remove('animate');
+    }, 300);
 });
 lv1.addEventListener('click', function () {
-    if (count <10) {
-        alert("kasif")
-
-        return
+    if (count < 10) {
+        alert("kasif");
+        return;
     }
-        inc = inc+1
-        count-=10
+    inc += 1;
+    count -= 10;
     counter.textContent = count;
 });
 lv2.addEventListener('click', function () {
-    if (count <20) {
-        alert("kasif")
-        return
+    if (count < 20) {
+        alert("kasif");
+        return;
     }
-    inc = inc+3
-    count-=20
+    inc += 2;
+    count -= 20;
     counter.textContent = count;
 });
 lv3.addEventListener('click', function () {
-    if (count <30) {
-        alert("kasif")
-
-        return
+    if (count < 30) {
+        alert("kasif");
+        return;
     }
-    inc = inc+5
-    count-=30
+    inc += 5;
+    count -= 30;
     counter.textContent = count;
 });
-// img.addEventListener('click', function () {
-//     count++;
-//     counter.textContent = count;
-//     if (count >= 30) {
-//         count += 5;
-//     }
-// });
